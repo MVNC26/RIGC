b36a18667eec2de7e79356af2d861f129c782211
MVNC26 <djavuaxebahia@gmail.com>
2024-04-23 16:58:14 -0300

    feat: animação dos textos

---
+++ script.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.texto-quemsomos',{
+    rotate: {x:0, y:-80, z:0},
+    duration:1000
+})
+
+sr.reveal('.texto-oquefazemos', {
+    rotate: {x:0, y:80, z:0},
+    duration:1000
+})
+
+
+sr.reveal('.conteudo-duvidas',{
+    rotate: {x:100, y:0, z:0},
+    duration:1000
+}) 
+
+
+sr.reveal('.texto-portfolio',{
+    rotate: {x:0, y:0, z:100},
+    duration:1000
+})
